e3c85f4d8e8816d2566ce5e6e336cac5d5962203
Minton <mintontiew@hotmail.com>
2022-06-06 12:45:53 +0800

    Cleaned up UI for N1, Fuel Cell Temperature, Power, Fuel Flow, and Speedometer. UI cycle test now cycles all parts of the UI at once.

--- wheel_ui.py
+++ wheel_ui.py
@@ -63,7 +63,7 @@ class RotatableLine:
         self.start_x = self.end_x - (self.len * math.cos(self.angle))
         self.start_y = self.end_y - (self.len * math.sin(self.angle))
 
-        print(self.start_x, self.start_y, self.end_x, self.end_y)
+        #print(self.start_x, self.start_y, self.end_x, self.end_y)
 
         self.line = self.parent.create_line(self.start_x, self.start_y, self.end_x, self.end_y)
     
@@ -97,6 +97,10 @@ for row in range (3):
 for col in range(3):
     dashboard.columnconfigure(col, weight=1)
 
+
+text_label_font = ("Futura", 48)
+text_status_font = ("Futura", 36)
+text_label_color = "white"
 arc_line_width = 3
 arc_line_colour = "white"
 arc_fill_colour = "grey"
@@ -109,51 +113,56 @@ spdo_line_colour = "white"
 spdo_colour = "cyan"
 circle_colour = "blue"
 circle_line_width = 3
-s.configure("MyStyle.TLabel", relief="solid", background="black", foreground='white', anchor=tk.CENTER, font=('Helvetica', 24))
-s.configure("Spdo.TLabel", background="black", foreground='white', anchor=tk.CENTER, font=('Helvetica', 40))
-s.configure("MyStyle1.TLabel", relief="solid", background="black", foreground='white', anchor=tk.CENTER, font=('Helvetica', 20))
+s.configure("MyStyle.TLabel", relief="solid", background='black', foreground='white', anchor=tk.CENTER, font=('Helvetica', 24))
+s.configure("Spdo.TLabel", background='black', foreground='white', anchor=tk.CENTER, font=('Helvetica', 40))
+s.configure("MyStyle1.TLabel", relief="solid", background="black", anchor=tk.CENTER, font=('Helvetica', 20))
 s.configure("Units.TLabel", background="black", foreground='blue', anchor=tk.CENTER, font=('Helvetica', 20))
 # s.configure("MyStyle.TButton", background="black", foreground='blue', anchor=tk.CENTER, font=('Helvetica', 20))
 
 
-# N1
+######## N1 ########
 frame_N1 = ttk.Frame(dashboard, style="MyStyle.TFrame")
-print("hi")
 print(frame_N1.winfo_width())
 frame_N1.grid(column=0, row=0, sticky="nsew")
-
 canvas_N1 = tk.Canvas(frame_N1, bg="black", highlightthickness=0)
 canvas_N1.pack(fill="both", expand=True)
 
+# Fill Arc
 arc_N1 = canvas_N1.create_arc(10, 0, 150, 140, outline=arc_fill_colour, fill=arc_fill_colour, width=arc_line_width, start=0, extent=-0)
 canvas_N1.create_arc(10, 0, 150, 140, outline=arc_line_colour, width=arc_line_width, start=0, extent=-190, style="arc")
 
-label_N1 = ttk.Label(canvas_N1, style="MyStyle.TLabel", text="error")
-canvas_N1.create_window(100, 10, width=156, height=58, anchor='nw', window=label_N1)
+# Box and value
+canvas_N1.create_polygon(200, 5,  80, 5,  80, 70,  200, 70,  outline='gray', width='3')
+label_N1 = canvas_N1.create_text(142, 37, width=200, text="N/A", anchor='center', font=text_status_font, fill=text_label_color)
 
-canvas_N1.create_text(180, 100, text='N1 %', anchor='nw', font=text_unit_font, fill=text_unit_colour)
 
 
-# FCT
+canvas_N1.create_text(160, 80, text='N1 %', anchor='nw', font=text_unit_font, fill=text_unit_colour)
+
+
+######## Fuel cell temperature ########
 frame_FCT = ttk.Frame(dashboard, style="MyStyle.TFrame")
 frame_FCT.grid(column=0, row=1, sticky="nsew")
-
 canvas_FCT = tk.Canvas(frame_FCT, bg="black", highlightthickness=0)
 canvas_FCT.pack(fill="both", expand=True)
 
+# Fill Arc
 arc_FCT = canvas_FCT.create_arc(10, 0, 150, 140, outline=arc_fill_colour, fill=arc_fill_colour, width=arc_line_width, start=0, extent=-0)
 canvas_FCT.create_arc(10, 0, 150, 140, outline=arc_line_colour, width=arc_line_width, start=0, extent=-190, style="arc")
 
-label_FCT = ttk.Label(canvas_FCT, style="MyStyle.TLabel", text="error")
-canvas_FCT.create_window(100, 10, width=156, height=58, anchor='nw', window=label_FCT)
+# Box and value
+canvas_FCT.create_polygon(200, 5,  80, 5,  80, 70,  200, 70,  outline='gray', width='3')
+label_FCT = canvas_FCT.create_text(142, 37, width=200, text="N/A", anchor='center', font=text_status_font, fill=text_label_color)
 
-canvas_FCT.create_text(150, 100, text='FCT \N{DEGREE SIGN}C', anchor='nw', font=text_unit_font, fill=text_unit_colour)
+canvas_FCT.create_text(160, 80, text='FCT \N{DEGREE SIGN}C', anchor='nw', font=text_unit_font, fill=text_unit_colour)
 
 
 
-# PWR_FF
+######## Power and fuel flow ########
 frame_PWR_FF = ttk.Frame(dashboard, style="MyStyle.TFrame")
 frame_PWR_FF.grid(column=0, row=2, sticky="nsew")
+canvas_PWR_FF = tk.Canvas(frame_PWR_FF, bg="black", highlightthickness=0)
+canvas_PWR_FF.pack(fill="both", expand=True)
 
 # create 2x2 grid
 for row in range (2):
@@ -161,49 +170,42 @@ for row in range (2):
 for col in range(2):
     frame_PWR_FF.columnconfigure(col, weight=1, minsize=130)
 
-label_PWR = ttk.Label(frame_PWR_FF, style="MyStyle1.TLabel", text="error")
-label_PWR.grid(column=0, row=0, padx=10, pady=10, sticky="nsew")
 
-label_FF = ttk.Label(frame_PWR_FF, style="MyStyle1.TLabel", text="error")
-label_FF.grid(column=0, row=1, padx=10, pady=10, sticky="nsew")
+canvas_PWR_FF.create_polygon(130, 5,  10, 5,  10, 70,  130, 70,  outline='gray', width='3')
+label_PWR = canvas_PWR_FF.create_text(72, 37, width=200, text="N/A", anchor='center', font=text_status_font, fill=text_label_color)
+canvas_PWR_FF.create_text(145, 30, text='PWR x W', anchor='nw', font=text_unit_font, fill=text_unit_colour)
 
-label_PWR_unit = ttk.Label(frame_PWR_FF, style="Units.TLabel", text="PWR X W")
-label_PWR_unit.grid(column=1, row=0, sticky="nsew")
-
-label_FF_unit = ttk.Label(frame_PWR_FF, style="Units.TLabel", text="FF/MIN")
-label_FF_unit.grid(column=1, row=1, sticky="nsew")
+canvas_PWR_FF.create_polygon(130, 75,  10, 75,  10, 135,  130, 135,  outline='gray', width='3')
+label_FF = canvas_PWR_FF.create_text(72, 107, width=200, text="N/A", anchor='center', font=text_status_font, fill=text_label_color)
+canvas_PWR_FF.create_text(145, 100, text='FF / MIN', anchor='nw', font=text_unit_font, fill=text_unit_colour)
 
 
 
-# SPD
+######## Speedometer ########
 frame_SPD = ttk.Frame(dashboard, style="MyStyle.TFrame")
 frame_SPD.grid(column=1, row=0, sticky="nsew", rowspan=2, columnspan=2)
 
-canvas_SPD = tk.Canvas(frame_SPD, bg="black", highlightthickness=0)
+canvas_SPD = tk.Canvas(frame_SPD, background="black", highlightthickness=0, selectbackground='black')
 canvas_SPD.pack(fill="both", expand=True)
 
 canvas_SPD.create_arc(10, 10, 520, 520, outline=spdo_colour, width=spdo_line_width, start=0, extent=180, style="arc")
-# test_line = canvas_SPD.create_line(250, 260, 300, 260)
-# canvas_SPD.itemconfigure(test_line, fill=spdo_colour, width=spdo_line_width)
 
 for a in range(0,181,18):
     segment = RotatableLine(canvas_SPD, 265, 265, 255, 20, a)
     canvas_SPD.itemconfigure(segment.line, fill=spdo_colour, width=spdo_line_width)
 
-# seg_1 = create_arc_seg(canvas_SPD, 265, 265, 255, 200, 15)
+
 needle = RotatableLine(canvas_SPD, 265, 265, 255, 50, 180)
 canvas_SPD.itemconfigure(needle.line, fill=spdo_line_colour, width=spdo_line_width)
 
-speed_reading = ttk.Label(canvas_SPD, style="Spdo.TLabel", text="error")
-canvas_SPD.create_window(160, 120, width=200, height=80, anchor='nw', window=speed_reading)
-canvas_SPD.create_text(160, 200, text='SPD X KM/H', anchor='nw', font=text_unit_font, fill=text_unit_colour)
-
-
+speed_reading = canvas_SPD.create_text(260, 100, width=400, text="NO DATA", anchor='n', font=text_label_font, fill=text_label_color)
+canvas_SPD.create_text(200, 180, text='SPD X KM/H', anchor='nw', font=text_unit_font, fill=text_unit_colour)
+status_message = canvas_SPD.create_text(260, 220, width=400, text="STATUS", anchor='n', font=text_status_font, fill=text_label_color)
 
 def start_FC():
-    print("start fuel cell")
+    fcc.start_fuel_cell()
 
-# ST_FC
+######## Buttons ######## 
 frame_ST_FC = ttk.Frame(dashboard, style="MyStyle.TFrame")
 frame_ST_FC.grid(column=1, row=2, sticky="nsew")
 
@@ -222,7 +224,7 @@ circle_y2 = circle_y1 + (2*circle_rad)
 circle_x2 = circle_x1 + (2*circle_rad)
 canvas_ST_FC.create_oval(circle_x1, circle_y1, circle_x2, circle_y2, outline=circle_colour, width=circle_line_width)
 
-print("canvas_ST_FC", ST_FC_width, ST_FC_height )
+#print("canvas_ST_FC", ST_FC_width, ST_FC_height )
 FC_button = tk.Button(canvas_ST_FC, relief="flat", bg="orange", fg="blue", font=('Helvetica', 24), text="Start\nFC", command=start_FC)
 canvas_ST_FC.create_window(0, 0, width=ST_FC_width, height=ST_FC_height, anchor='nw', window=FC_button)
 
@@ -249,37 +251,49 @@ for a in range(0,181,16):
     canvas_FUEL.itemconfigure(segment1.line, fill=spdo_colour, width=spdo_line_width)
 
 
+def update_status(string):
+    pass
 
-def inc_pie():
-    while (1):
-        for i in range(190):
-            canvas_N1.itemconfigure(arc_N1, extent=-i)
-            time.sleep(0.01)
-            label_N1["text"] = i/10
-            # print("hi")
-        print("loop")
-        for i in range(190):
-            canvas_FCT.itemconfigure(arc_FCT, extent=-i)
-            time.sleep(0.01)
-            label_FCT["text"] = i/10
-            # print("hi")
-        print("loop1")
-        for i in range(180,0,-1):
-            needle.rotate_line(i)
-            speed_reading["text"] = i/10
-            time.sleep(0.01)
-        print("loop2")
-        for i in range(0,1500,10):
-            label_PWR.config(text=int(fcc.recv_fuel_cell()/150))
-            label_FF.config(text=i/100)
-            time.sleep(0.01)
-        print("loop3")
 
-thread1 = threading.Thread(target=inc_pie, daemon=True)
+def main_ui_loop():
+    while (True):
+        #pass
+        # code here
+        ui_cycle_test()
+
+
+        #interpret data
+
+
+def ui_cycle_test():
+    for i in range(101):
+
+        halfcircle_progress = (180*i)/100
+
+        canvas_N1.itemconfigure(arc_N1, extent=-halfcircle_progress)  # extent is angle in degrees
+        canvas_N1.itemconfigure(label_N1, text=i)
+        
+        canvas_FCT.itemconfigure(arc_FCT, extent=-halfcircle_progress) # extent is angle in degrees
+        
+        canvas_FCT.itemconfigure(label_FCT, text = i)
+
+        needle.rotate_line(180-halfcircle_progress)
+        canvas_SPD.itemconfigure(speed_reading, text=i)
+        
+
+        canvas_PWR_FF.itemconfigure(label_FF, text = i)
+        canvas_PWR_FF.itemconfigure(label_PWR, text = i)
+
+        time.sleep(0.01)
+        
+
+
+
+thread1 = threading.Thread(target=main_ui_loop, daemon=True)
 thread1.start()
 
-# thread2 = threading.Thread(target=fcc.recv_fuel_cell, daemon=True)
-# thread2.start()
+thread2 = threading.Thread(target=fcc.recv_fuel_cell, daemon=True)
+thread2.start()
 
 # keep the window displaying
 root.mainloop()
